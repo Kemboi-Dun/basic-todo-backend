52e349570bf214ee9dd21666ed69971e1eeb301f
Kemboi-Dun <gahrmicc@gmail.com>
2023-04-10 12:16:14 +0300

    Added cors headers

--- myproject/settings.py
+++ myproject/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders',
     'todo'
 ]
 
@@ -62,7 +63,16 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://basic-todo-backend-production.up.railway.app",
+    "http://localhost:8000",
+]
+
+CORS_ORIGIN_ALLOW_ALL = False
 
 ROOT_URLCONF = "myproject.urls"
 
